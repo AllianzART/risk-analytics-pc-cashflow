9e01e958d417187df070701cd0fd96a9923626af
Paolo Albini <paolo.albini@art-allianz.com>
2015-07-21 20:23:14 +0100

    [AR-111] Chronological order patch to deprecated ClaimUtils.aggregateByBaseClaimCDP

--- src/groovy/org/pillarone/riskanalytics/domain/pc/cf/claim/ClaimUtils.java
+++ src/groovy/org/pillarone/riskanalytics/domain/pc/cf/claim/ClaimUtils.java
@@ -229,9 +229,18 @@ public class ClaimUtils {
                 double reserved = 0;
                 double changeInReserves = 0;
 
+                DateTime mostRecentDate = new DateTime(0).minusYears(200); // better safe than sorry
+
                 for (ClaimDevelopmentPacket claim : claimsWithSameBaseClaim) {
-                    paid = claim.getPaid();
-                    reserved = claim.getReserved();
+
+                    if (claim.getDate().isAfter(mostRecentDate)) {
+
+                        paid = claim.getPaid();
+                        reserved = claim.getReserved();
+
+                        mostRecentDate = claim.getDate();
+
+                    }
                     // I'm not summing as I'd guess these not to be incremental but total values
                     //assumption would be that we are looking at updates to the same claim, in chronological order
                     changeInReserves += claim.getChangeInReserves(); //changes in reserves should be incremental by def
