f3bdbd1cbdb4aa94afc9fa55feb85999536255d8
fazl <art.frahman@gmail.com>
2016-01-05 18:07:59 +0100

    AR-111 Revert configs to master

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,5 +1,5 @@
 //Use a custom plugins dir, because different branches use different plugin versions
-grails.project.plugins.dir = "../local-plugins/risk-analytics-pc-cashflow-AR-111"
+grails.project.plugins.dir = "../local-plugins/risk-analytics-pc-cashflow-master"
 
 grails.project.dependency.resolver = "maven"
 
@@ -31,9 +31,9 @@ grails.project.dependency.resolution = {
         compile ":excel-import:1.0.0"
 
         if (appName == "risk-analytics-pc-cashflow") {
-            runtime "org.pillarone:risk-analytics-core:AR-111-SNAPSHOT"
-            runtime("org.pillarone:risk-analytics-commons:1.10.1") { transitive = false }
-            runtime("org.pillarone:risk-analytics-property-casualty:AR-111-SNAPSHOT") { transitive = false }
+            runtime "org.pillarone:risk-analytics-core:1.10-SNAPSHOT"
+            runtime("org.pillarone:risk-analytics-commons:1.10-SNAPSHOT") { transitive = false }
+            runtime("org.pillarone:risk-analytics-property-casualty:1.10-SNAPSHOT") { transitive = false }
         }
     }
 
@@ -45,9 +45,9 @@ grails.project.dependency.resolution = {
         }
     }
 }
-//grails.plugin.location.'risk-analytics-core' = "../risk-analytics-core-AR-111"
-//grails.plugin.location.'risk-analytics-property-casualty' = "../risk-analytics-property-casualty-AR-111"
-//grails.plugin.location.'risk-analytics-commons' = "../risk-analytics-commons-AR-111"
+//grails.plugin.location.'risk-analytics-core' = "../risk-analytics-core-master"
+//grails.plugin.location.'risk-analytics-commons' = "../risk-analytics-commons-master"
+//grails.plugin.location.'risk-analytics-property-casualty' = "../risk-analytics-property-casualty-master"
 
 grails.project.repos.default = "pillarone"
 
